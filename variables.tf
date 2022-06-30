3740ff7fa4c1b82817fd1356f1b56e1bed9181e1
ukesavalu <ukesavalu@msystechnologies.com>
2022-06-30 18:19:41 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name" {
-  default     = "res_grp"
+  default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
