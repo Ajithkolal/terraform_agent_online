35ebadc755863ee9e0eb57f0fd37bf7c253e249d
ukesavalu <ukesavalu@msystechnologies.com>
2022-06-30 16:13:08 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "resource_group_name_prefix" {
-  default     = "rg"
+variable "resource_group_name" {
+  default     = "res_grp"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
 variable "resource_group_location" {
-  default     = "eastasia"
+  default     = "centralus"
   description = "Location of the resource group."
 }
