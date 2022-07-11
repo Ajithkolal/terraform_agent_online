f51d3755a469facb6e3c51fb9f1efd24e87fa0fc
aonkarappa <aonkarappa@msystechnologies.com>
2022-07-11 06:26:50 +0000

    Updated script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ wget https://vstsagentpackage.azureedge.net/agent/2.204.0/vsts-agent-linux-x64-2
 cd /home/azureuser/myagent/
 tar zxvf /home/azureuser/Downloads/vsts-agent-linux-x64-2.204.0.tar.gz
 sudo chmod -R 777 /home/azureuser/myagent/
-runuser -l azureuser -c '/home/azureuser/myagent/config.sh --unattended  --url https://dev.azure.com/ukesavalu --auth pat --token jk3km7cpkvjk4cusz4us2j2l6w2prrbauk4kppptalbjp5msbq5a --pool udhaya_release_pool'
+runuser -l azureuser -c '/home/azureuser/myagent/config.sh --unattended  --url https://dev.azure.com/aonkarappa --auth pat --token iusnoqigdhhlmm6ny7wiiw3w3ubakfn5e6l3ykrqrk27ndzosxqq --pool ajith_pool'
 sudo /home/azureuser/myagent/svc.sh install
 sudo /home/azureuser/myagent/svc.sh start
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
